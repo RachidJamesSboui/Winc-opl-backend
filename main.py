fa4827e6c883474b03aed9aec2d9ce491ea5deed
Rachid James Sboui <79262664+RachidJamesSboui@users.noreply.github.com>
2021-02-25 20:39:01 +0100

    Add files via upload

---
+++ main.py
@@ -0,0 +1,18 @@
+# Do not modify these lines
+__winc_id__ = '71dd124b4a6e4d268f5973db521394ee'
+__human_name__ = 'strings'
+
+# Add your code after this line
+scorer_0 = 'Ruud Gullit'
+scorer_1 = 'Marco van Basten'
+goal_0 = 32
+goal_1 = 54
+scorers = 'Ruud Gullit' + ' 32, ' + 'Marco van Basten' + ' 54'
+report = f'{scorer_0} scored in the {goal_0}nd minute' + f'\n{scorer_1} scored in the {goal_1}th minute'
+
+player = 'Ruud Gullit'
+first_name = player[0:4]
+last_name_len = len(player[5:11])
+name_short = player[0] + '.' + ' ' + player[5:11]
+chant = 'Ruud! '*4 #geen idee wat ik hier volgens wincpy fout blijf doen
+good_chant = 'Ruud!'!='Ruud '
