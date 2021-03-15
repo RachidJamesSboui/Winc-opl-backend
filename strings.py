158d217cc97252d71adc5663a2e2a6e750ad1fbb
Rachid James Sboui <rachidsboui@outlook.com>
2021-03-15 18:09:17 +0100

    assignment her

--- strings.py
+++ strings.py
@@ -7,32 +7,28 @@ scorer_0 = 'Ruud Gullit'
 scorer_1 = 'Marco van Basten'
 goal_0 = 32
 goal_1 = 54
-scorers = scorer_0, str(goal_0), scorer_1, str(goal_1)
-report = f'{scorer_0} scored in the {goal_0}nd minute'f'\n{scorer_1} scored in the {goal_1}th minute'
+scorers = scorer_0 + ' ' + str(goal_0) + ', ' + scorer_1 + ' ' + str(goal_1)
+report = f'{scorer_0} scored in the {goal_0}nd minute\n{scorer_1} scored in the {goal_1}th minute'
 
 print(scorers)
 print(report)
 
 player = 'Ruud Gullit'
-first_name = player.find(' ')
-last_name = player[5:11]
-
-'''ik heb uren gegoogled en verdwaald geraakt in tutorial hell, 
-   ik kan helaas nergens vinden hoe ik voor last_name zoek vanaf de spatie (' ') tot het laatste karakter 
-   of hoe ik .find .len .[] in 1 regel mag en kan gebruiken'''
-
+first_name = player[:player.find(' ')]
+last_name = player[player.find(' '):-1]
 last_name_len = len(last_name)
-name_short = player[0] + '. ' + player[5:11]
-chant = (player[0:4]+'! ')*first_name
-good_chant = chant.find(' ') != ' '
-print(player)
+
 print(first_name)
 print(last_name)
 print(last_name_len)
-print(name_short)
-print(chant)
-print(good_chant)
 
-''' Is het mogelijk dat men het antwoord geeft, of laat zien wat er bij last_name_len en scorers zou moeten staan
-    Want ik zit nu ruim een week vast op een zeer kleine opdracht en ik begrijp gewoon niet wat er nu van me gevraagd
-    word daar ik wel het idee heb dat ik de stof voldoende begrijp en toepas'''
+'''heb hier dus geprogrammeerd dat ik de lengte van de achternaam wil, -1
+want de spatie wordt altijd meegeteld'''
+
+name_short = player[0] + '. ' + player[5:11]
+
+chant = (player[0:4]+'! ')*4
+print(chant)
+
+good_chant = chant[-1] != ' '
+print(good_chant)
